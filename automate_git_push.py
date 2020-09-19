56206ba86a5700cd9091afdaeeebb5b1e9106edb
AparnaEm16 <aparnaiyer1695@gmail.com>
2020-09-19 23:42:42 +0530

    Updated 19/09/2020 23:42:32

--- automate_git_push.py
+++ automate_git_push.py
@@ -7,15 +7,7 @@ from datetime import datetime
 os.chdir(os.path.abspath('/home/aparna/PycharmProjects/myFirstProject'))
 repo_1_pvt_url="git@github.com:AparnaSIyer/Repo_backup_1.git"
 repo_2_pub_url="git@github.com:AparnaSIyer/Repo_backup_2.git"
-#pullling the latest code changes to above repos
-def push_automate_git_repo():
-    # Popen(['git', 'clone', str('git@github.com:AparnaSIyer/Repo_backup_1.git'), '/home/aparna/Repo_backup_1'])
-    # Popen(['git', 'clone', str('git@github.com:AparnaSIyer/Repo_backup_2.git'), '/home/aparna/Repo_2'])
-    print(call("git remote set-url origin 'git@github.com:AparnaSIyer/Repo_backup_1.git'", shell=True))
-    print(Popen(['git', 'pull', str('git@github.com:AparnaSIyer/automateGitPullPush.git')]))
-    print(Popen('git push',shell=True))
-    time.sleep(5)
-push_automate_git_repo()
+
 #hello bro 1
 Popen(['git','pull'])
 Popen(['git', 'add', '--all'])
@@ -30,3 +22,12 @@ print(call(['git', 'remote', '-v']))
 (Popen('git push', shell=True))
 #Test whether the pull command is working via Github
 #test test test
+#pullling the latest code changes to above repos
+def push_automate_git_repo():
+    # Popen(['git', 'clone', str('git@github.com:AparnaSIyer/Repo_backup_1.git'), '/home/aparna/Repo_backup_1'])
+    # Popen(['git', 'clone', str('git@github.com:AparnaSIyer/Repo_backup_2.git'), '/home/aparna/Repo_2'])
+    print(call("git remote set-url origin 'git@github.com:AparnaSIyer/Repo_backup_1.git'", shell=True))
+    print(Popen(['git', 'pull', str('git@github.com:AparnaSIyer/automateGitPullPush.git')]))
+    print(Popen('git push',shell=True))
+    time.sleep(5)
+push_automate_git_repo()
