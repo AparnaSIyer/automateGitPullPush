fce6b849dccdd3dad32006bd6d67c1a10a504a0f
AparnaEm16 <aparnaiyer1695@gmail.com>
2020-09-19 23:24:56 +0530

    Updated 19/09/2020 23:24:46

--- automate_git_push.py
+++ automate_git_push.py
@@ -11,8 +11,6 @@ repo_2_pub_url="git@github.com:AparnaSIyer/Repo_backup_2.git"
 def push_automate_git_repo():
     # Popen(['git', 'clone', str('git@github.com:AparnaSIyer/Repo_backup_1.git'), '/home/aparna/Repo_backup_1'])
     Popen(['git', 'clone', str('git@github.com:AparnaSIyer/Repo_backup_2.git'), '/home/aparna/Repo_2'])
-    print(call("git remote set-url origin 'git@github.com:AparnaSIyer/Repo_backup_2.git'", shell=True))
-    print(call("git remote set-url origin 'git@github.com:AparnaSIyer/Repo_backup_1.git'", shell=True))
     print(Popen(['git', 'pull', str('git@github.com:AparnaSIyer/automateGitPullPush.git')]))
     print(Popen('git push',shell=True))
     time.sleep(5)
