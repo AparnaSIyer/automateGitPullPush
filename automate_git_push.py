0f44064929a120c6f59f7743d2dc28c56e725da5
AparnaEm16 <aparnaiyer1695@gmail.com>
2020-09-19 14:14:09 +0530

    Can be changed

--- automate_git_push.py
+++ automate_git_push.py
@@ -1,15 +1,19 @@
 from subprocess import *
 import os
+import time
+
 
 # Popen(['git','clone',str('https://github.com/AparnaSIyer/hello-world'),'/home/aparna/my_project_1'])
 os.chdir(os.path.abspath('/home/aparna/PycharmProjects/myFirstProject'))
 
 Popen(['git', 'add', '--all'])
 message = "Can be changed"
+time.sleep(10)
 Popen(['git', 'commit', '-m', message])
 remote_url = 'git@github.com:AparnaSIyer/automateGitPullPush.git'
 remote_url2 = 'git@github.com:AparnaSIyer/hello-world.git'
 print(call("git remote set-url origin 'git@github.com:AparnaSIyer/automateGitPullPush.git'", shell=True))
 print(call(['git', 'remote', '-v']))
 # print('setting remote repo')
+
 (Popen('git push', shell=True))
